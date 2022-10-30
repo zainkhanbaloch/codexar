863542927570a5a7df8ffc77dffeb5c3c386955a
“Zain_Baloch” <mzainabbas77@gmail.com>
2022-10-30 12:00:26 +0500

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  theme: {
+    extend: {    
+        
+    },
+  },
+  plugins: [],
+};
